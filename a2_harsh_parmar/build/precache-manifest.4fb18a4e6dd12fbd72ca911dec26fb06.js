bc97a74dafe034dd6ebfc8e4bc323263fdb01ba9
Harsh <hr822264@dal.ca>
2020-06-14 19:30:06 -0300

    build

---
+++ a2_harsh_parmar/build/precache-manifest.4fb18a4e6dd12fbd72ca911dec26fb06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40a77982d123574ff71070de5edc4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b82196d6c08d566bd64",
+    "url": "/static/css/2.e0e915a7.chunk.css"
+  },
+  {
+    "revision": "f6d66ebe6f43d14917e4",
+    "url": "/static/css/main.f79fbfc0.chunk.css"
+  },
+  {
+    "revision": "2b82196d6c08d566bd64",
+    "url": "/static/js/2.22193a7b.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/static/js/2.22193a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d66ebe6f43d14917e4",
+    "url": "/static/js/main.e2cd1cc6.chunk.js"
+  },
+  {
+    "revision": "072faab0aa1d5326278e",
+    "url": "/static/js/runtime-main.3d0dedb0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8de90950c1e2bd56f0ea171b30262968",
+    "url": "/static/media/userlogo.8de90950.png"
+  }
+]);
